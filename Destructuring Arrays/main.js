6e7d75a3452cd1f82342e7f3135b39c3ad7b256b
tungtokuda <tungnguyen12092003@gmail.com>
2023-01-11 14:48:17 +0700

    abc

---
+++ Destructuring Arrays/main.js
@@ -0,0 +1,6 @@
+// Destructuring là một cú pháp cho phép bạn gán các thuộc tính của một Object hoặc một Array.
+// Điều này có thể làm giảm đáng kể các dòng mã cần thiết để thao tác dữ liệu trong các cấu trúc này.
+// Có hai loại Destructuring: Destructuring Objects và Destructuring Arrays.
+
+// const [a, b] = [1, 2]
+// console.log(a, b); //hien : 1 2
